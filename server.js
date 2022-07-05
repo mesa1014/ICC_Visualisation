7a6995accc0fafa44cfca595e5463402b8c26de0
mesa1014 <mehrdad.sangi@gmail.com>
2022-07-06 10:19:17 +1200

    Added Heroku requirements

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.use(express.static('public'))
 //     res.sendFile(__dirname + "/index.html");
 // });
 
-app.listen(8082, function () {
-    console.log("Server is running on localhost:8082");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
